3aa3d9805e6ac721d38c90ab86b4dc1ffb00b2b6
lbarrios <liliana.usb@gmail.com>
2015-08-06 13:32:17 +0200

    first files

---
+++ app/src/main/java/com/bertha/interactivestory/StoryActivity.java
@@ -0,0 +1,25 @@
+package com.bertha.interactivestory;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class StoryActivity extends ActionBarActivity {
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(getString(R.string.key_name)); //use getString to avoid hardcoded keys
+        Log.d(TAG, name);
+    }
+
+}
